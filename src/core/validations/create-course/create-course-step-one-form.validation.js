bc87bd65b80c73fdc8779daf81de0a524fe28817
Mohammad <bhshtbaran@gmail.com>
2024-05-25 14:14:28 -0700

    add required fields in validation of create course step two form

--- src/core/validations/create-course/create-course-step-one-form.validation.js
+++ src/core/validations/create-course/create-course-step-one-form.validation.js
@@ -1,9 +1,9 @@
 import * as yup from "yup";
 
 export const createCourseStepOneFormSchema = yup.object().shape({
-  title: yup.string(),
-  cost: yup.string(),
-  capacity: yup.string(),
-  sessionNumber: yup.string(),
-  miniDescribe: yup.string(),
+  title: yup.string().required("این فیلد الزامی می باشد"),
+  cost: yup.string().required("این فیلد الزامی می باشد"),
+  capacity: yup.string().required("این فیلد الزامی می باشد"),
+  sessionNumber: yup.string().required("این فیلد الزامی می باشد"),
+  miniDescribe: yup.string().required("این فیلد الزامی می باشد"),
 });

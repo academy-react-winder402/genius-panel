38ca1b5efacd57b23ddd4f06a0ba0023ab2d6da8
malja1500 <mianroudianf@gmail.com>
2024-06-09 16:05:35 +0330

    fix some bugs

--- src/core/validations/create-blog/creat-blog.js
+++ src/core/validations/create-blog/creat-blog.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const createCourseStepOneFormSchema = yup.object().shape({
+export const createBlogFormValidation = yup.object().shape({
   title: yup.string().required("این فیلد الزامی می باشد"),
   miniDescribe: yup.string().required("این فیلد الزامی می باشد"),
   googleTitle: yup.string().required("این فیلد الزامی می باشد"),

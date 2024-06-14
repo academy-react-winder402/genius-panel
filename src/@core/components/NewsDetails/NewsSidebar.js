0d6d65b9d6edd0f26f9eaf36e78a52c1370541b6
Mohammad <bhshtbaran@gmail.com>
2024-06-14 01:57:54 -0700

    Implement recent news in sidebar of news details page

--- src/@core/components/NewsDetails/NewsSidebar.js
+++ src/@core/components/NewsDetails/NewsSidebar.js
@@ -1,23 +1,41 @@
 // ** React Imports
 import { Fragment, useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 // ** Core Imports
+import { adminNewsFilterListAPI } from "../../../core/services/api/news/admin-news-filter-list.api";
 import { getNewsCategoryListsAPI } from "../../../core/services/api/news/get-news-category-lists";
 
+// ** Utils
+import { convertDateToPersian } from "../../../core/utils/date-helper.utils";
+
 // ** Third Party Components
-import { CheckCircle, Star } from "react-feather";
+import classnames from "classnames";
+import { Star } from "react-feather";
 
 // ** Custom Components
 import Avatar from "@components/avatar";
 
-// ** Reactstrap Imports
+// ** Images
+import blankThumbnail from "../../../assets/images/common/blank-thumbnail.jpg";
 
 const NewsSidebar = () => {
   // ** States
+  const [recentNews, setRecentNews] = useState();
   const [newsCategoryLists, setNewsCategoryLists] = useState();
 
   useEffect(() => {
+    const fetchRecentNews = async () => {
+      try {
+        const getRecentNews = await adminNewsFilterListAPI(1, 4);
+
+        setRecentNews(getRecentNews);
+      } catch (error) {
+        toast.error("مشکلی در دریافت آخرین اخبار به وجود آمد !");
+      }
+    };
+
     const fetchNewsCategoryLists = async () => {
       try {
         const getNewsCategoryLists = await getNewsCategoryListsAPI();
@@ -26,53 +44,59 @@ const NewsSidebar = () => {
 
         setNewsCategoryLists(getLatestNewsCategoryLists);
       } catch (error) {
-        console.log(error);
         toast.error("مشکلی در دریافت لیست دسته بندی های اخبار به وجود آمد !");
       }
     };
 
+    fetchRecentNews();
     fetchNewsCategoryLists();
   }, []);
 
-  // const renderRecentPosts = () => {
-  //   return data.recentPosts.map((post, index) => {
-  //     return (
-  //       <div
-  //         key={index}
-  //         className={classnames("d-flex", {
-  //           "mb-2": index !== data.recentPosts.length - 1,
-  //         })}
-  //       >
-  //         <Link className="me-2" to={`/pages/blog/detail/${post.id}`}>
-  //           <img
-  //             className="rounded"
-  //             src={post.img}
-  //             alt={post.title}
-  //             width="100"
-  //             height="70"
-  //           />
-  //         </Link>
-  //         <div>
-  //           <h6 className="blog-recent-post-title">
-  //             <Link
-  //               className="text-body-heading"
-  //               to={`/pages/blog/detail/${post.id}`}
-  //             >
-  //               {post.title}
-  //             </Link>
-  //           </h6>
-  //           <div className="text-muted mb-0">{post.createdTime}</div>
-  //         </div>
-  //       </div>
-  //     );
-  //   });
-  // };
+  const renderRecentNews = () => {
+    return recentNews?.news.map((news) => {
+      const formattedUpdateDate = convertDateToPersian(news.updateDate);
 
-  const renderCategories = () => {
-    return newsCategoryLists?.map((item, index) => {
       return (
         <div
-          key={index}
+          key={news.id}
+          className={classnames("d-flex", {
+            "mb-2": news.id !== recentNews.totalCount - 1,
+          })}
+        >
+          <Link className="me-2" to={`/news/${news.id}`}>
+            <img
+              className="rounded"
+              src={
+                news.currentImageAddressTumb !== null
+                  ? news.currentImageAddressTumb
+                  : blankThumbnail
+              }
+              alt={news.title}
+              width="60"
+              height="60"
+            />
+          </Link>
+          <div>
+            <h6 className="blog-recent-post-title">
+              <Link
+                className="text-body-heading text-truncate recent-news-title"
+                to={`/news/${news.id}`}
+              >
+                {news.title}
+              </Link>
+            </h6>
+            <div className="text-muted mb-0">{formattedUpdateDate}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  const renderCategories = () => {
+    return newsCategoryLists?.map((item) => {
+      return (
+        <div
+          key={item.id}
           className="d-flex justify-content-start align-items-center"
         >
           <a className="me-75" href="/" onClick={(e) => e.preventDefault()}>
@@ -99,6 +123,10 @@ const NewsSidebar = () => {
           <div className="right-sidebar-content">
             {newsCategoryLists !== null ? (
               <Fragment>
+                <div className="blog-recent-posts mt-3">
+                  <h6 className="section-label">آخرین اخبار</h6>
+                  <div className="mt-75">{renderRecentNews()}</div>
+                </div>
                 <div className="blog-categories mt-3">
                   <h6 className="section-label">دسته بندی های اخبار</h6>
                   <div className="mt-1 d-flex flex-column gap-1">

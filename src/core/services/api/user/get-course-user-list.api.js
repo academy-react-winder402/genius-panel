ce3a68482c5a611f7ca6c1ab5504cfd034d16743
Mohammad <bhshtbaran@gmail.com>
2024-06-17 02:48:00 -0700

    Implement get course user list API

---
+++ src/core/services/api/user/get-course-user-list.api.js
@@ -0,0 +1,27 @@
+import http from "../../interceptor";
+
+export const getCourseList = async (
+  courseId,
+  pageNumber,
+  rowsOfPage,
+  sortingCol,
+  sortType,
+  query
+) => {
+  try {
+    const response = await http.get("/CourseUser/GetCourseUserList", {
+      params: {
+        courseId,
+        pageNumber,
+        rowsOfPage,
+        sortingCol,
+        sortType,
+        query,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
